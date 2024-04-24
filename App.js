5c3968b05b90fb177a0e4fd662dde4321a1a8906
Mahavishnu Jn <mahajneesh@hotmail.com>
2024-04-23 21:21:40 -0300

    init Projetct

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import { Appbar } from "react-native-paper";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
-export default function App() {
+const MyComponent = () => {
+  const _goBack = () => console.log("Went back");
+
+  const _handleSearch = () => console.log("Searching");
+
+  const _handleMore = () => console.log("Shown more");
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <Appbar.Header>
+        <Appbar.BackAction onPress={_goBack} />
+        <Appbar.Content title="Title" />
+        <Appbar.Action icon="magnify" onPress={_handleSearch} />
+        <Appbar.Action icon="dots-vertical" onPress={_handleMore} />
+      </Appbar.Header>
+    </SafeAreaProvider>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default MyComponent;
