4965a1ecbbe87a39bde935397db9d93a05881b8a
Mahavishnu Jn <mahajneesh@hotmail.com>
2024-04-23 22:05:47 -0300

    init the structure

--- App.js
+++ App.js
@@ -1,24 +1,61 @@
-import * as React from "react";
-import { Appbar } from "react-native-paper";
+import React, { useState } from "react";
+import { Appbar, TextInput, Button, Text } from "react-native-paper";
+import { StyleSheet, View } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
-const MyComponent = () => {
-  const _goBack = () => console.log("Went back");
-
-  const _handleSearch = () => console.log("Searching");
-
-  const _handleMore = () => console.log("Shown more");
-
+const App = () => {
+  const [gas, setGas] = useState("");
+  const [eta, setEta] = useState("");
+  const [res, setRes] = useState("");
   return (
     <SafeAreaProvider>
-      <Appbar.Header>
-        <Appbar.BackAction onPress={_goBack} />
-        <Appbar.Content title="Title" />
-        <Appbar.Action icon="magnify" onPress={_handleSearch} />
-        <Appbar.Action icon="dots-vertical" onPress={_handleMore} />
-      </Appbar.Header>
+      <View style={styles.container}>
+        <Appbar.Header>
+          <Appbar.Content style={styles.title} title="Calculadora Flex" />
+        </Appbar.Header>
+        <View style={styles.body}>
+          <TextInput
+            style={styles.input}
+            label="Preço da Gasolina"
+            value={gas}
+            onChangeText={(gas) => setGas(gas)}
+          />
+          <TextInput
+            style={styles.input}
+            label="Preço do Etanol"
+            value={eta}
+            onChangeText={(eta) => setEta(eta)}
+          />
+          <Button mode="contained" onPress={() => console.log("pass")}>
+            Calcular
+          </Button>
+          <Text style={styles.text}>{gas}</Text>
+        </View>
+      </View>
     </SafeAreaProvider>
   );
 };
 
-export default MyComponent;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#CCC",
+  },
+  body: {
+    margin: 8,
+  },
+  input: {
+    backgroundColor: "#FFF",
+    marginBottom: 8,
+  },
+  title: {
+    backgroundColor: "#cc2",
+    alignItems: "center",
+  },
+  text: {
+    textAlign: "center",
+    margin: 8,
+  },
+});
+
+export default App;
